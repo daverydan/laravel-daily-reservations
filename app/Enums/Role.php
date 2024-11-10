2b4804e54950c72315b82ebce9abe0a9c1c1e545
daverydan <danny.w.avery@gmail.com>
2024-11-10 17:45:41 -0500

    +toString to Role enum

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -8,4 +8,14 @@ enum Role: int
     case COMPANY_OWNER = 2;
     case CUSTOMER = 3;
     case GUIDE = 4;
+
+    public static function toString(int $type): string
+    {
+        return match ($type) {
+            self::ADMINISTRATOR->value => 'administrator',
+            self::COMPANY_OWNER->value => 'company owner',
+            self::CUSTOMER->value => 'customer',
+            self::GUIDE->value => 'guide',
+        };
+    }
 }
