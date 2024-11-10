089b01f6a70470e2223a3df2943aa40ad7ebf082
daverydan <danny.w.avery@gmail.com>
2024-11-10 18:41:26 -0500

    +Company Feature Test

---
+++ tests/Feature/CompanyTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CompanyTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_admin_user_can_access_companies_index_page(): void
+    {
+        $adminUser = User::factory()->admin()->create();
+
+        $response = $this->actingAs($adminUser)->get(route('companies.index'));
+
+        $response->assertOk();
+    }
+
+    public function test_non_admin_user_cannot_access_companies_index_page(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('companies.index'));
+
+        $response->assertForbidden();
+    }
+}
