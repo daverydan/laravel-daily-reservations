9f41527089d104b9ec0650d851d1dec882c2ec84
daverydan <danny.w.avery@gmail.com>
2024-11-10 18:48:05 -0500

    Re-configure testing database

--- config/database.php
+++ config/database.php
@@ -62,7 +62,7 @@ return [
             ]) : [],
         ],
 
-        'laravel_testing' => [
+        'testing' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
