b7124aa440e6821bb17bf252eecc8ecee27d40eb
daverydan <danny.w.avery@gmail.com>
2024-11-10 15:29:12 -0500

    + default role to newly registered user

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enums\Role;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
@@ -39,6 +40,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role_id' => Role::CUSTOMER->value,
         ]);
 
         event(new Registered($user));
