0a73bd036c7b81252dc865a9315fd5b4f285890e
daverydan <danny.w.avery@gmail.com>
2024-11-12 22:20:13 -0500

    +authorization for CompanyUserCtrl methods

--- app/Http/Controllers/CompanyUserController.php
+++ app/Http/Controllers/CompanyUserController.php
@@ -7,11 +7,14 @@ use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Models\Company;
 use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 
 class CompanyUserController extends Controller
 {
     public function index(Company $company)
     {
+        Gate::authorize('viewAny', $company);
+
         $users = $company->users()->where('role_id', Role::COMPANY_OWNER->value)
             ->get();
 
@@ -20,11 +23,15 @@ class CompanyUserController extends Controller
 
     public function create(Company $company)
     {
+        Gate::authorize('create', $company);
+
         return view('companies.users.create', compact('company'));
     }
 
     public function store(StoreUserRequest $request, Company $company)
     {
+        Gate::authorize('create', $company);
+
         $company->users()->create([
             ...$request->validated(),
             'password' => bcrypt($request->password),
@@ -36,11 +43,15 @@ class CompanyUserController extends Controller
 
     public function edit(Company $company, User $user)
     {
+        Gate::authorize('update', $company);
+
         return view('companies.users.edit', compact('company', 'user'));
     }
 
     public function update(UpdateUserRequest $request, Company $company, User $user)
     {
+        Gate::authorize('update', $company);
+
         $user->update($request->validated());
 
         return to_route('companies.users.index', $company);
@@ -48,6 +59,8 @@ class CompanyUserController extends Controller
 
     public function destroy(Company $company, User $user)
     {
+        Gate::authorize('delete', $company);
+
         $user->delete();
 
         return to_route('companies.users.index', $company);
