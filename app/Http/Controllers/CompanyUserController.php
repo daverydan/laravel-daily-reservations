eaeeb34de06be06df09988047f73282f0fd98afa
daverydan <danny.w.avery@gmail.com>
2024-11-12 20:15:23 -0500

    Create & Edit Company Users

--- app/Http/Controllers/CompanyUserController.php
+++ app/Http/Controllers/CompanyUserController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Enums\Role;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
 use App\Models\Company;
+use App\Models\User;
 
 class CompanyUserController extends Controller
 {
@@ -13,5 +16,33 @@ class CompanyUserController extends Controller
             ->get();
 
         return view('companies.users.index', compact('company', 'users'));
+    }
+
+    public function create(Company $company)
+    {
+        return view('companies.users.create', compact('company'));
+    }
+
+    public function store(StoreUserRequest $request, Company $company)
+    {
+        $company->users()->create([
+            ...$request->validated(),
+            'password' => bcrypt($request->password),
+            'role_id' => Role::COMPANY_OWNER->value,
+        ]);
+
+        return to_route('companies.users.index', $company);
+    }
+
+    public function edit(Company $company, User $user)
+    {
+        return view('companies.users.edit', compact('company', 'user'));
+    }
+
+    public function update(UpdateUserRequest $request, Company $company, User $user)
+    {
+        $user->update($request->validated());
+
+        return to_route('companies.users.index', $company);
     }
 }
