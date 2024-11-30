0e51d1dc288b5d1b1ba9cca02e9751e5590cbe29
daverydan <danny.w.avery@gmail.com>
2024-11-30 12:35:41 -0500

    Clean up routes

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::get('/', HomeController::class)->name('home');
 
 Route::middleware('auth')->group(function () {
