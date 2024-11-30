6c08ba54d92ed30dcb6422dbf70d9ce74e2a83f4
daverydan <danny.w.avery@gmail.com>
2024-11-30 13:53:16 -0500

    +ActivityObserver

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use App\Observers\ActivityObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+#[ObservedBy(ActivityObserver::class)]
 class Activity extends Model
 {
     use HasFactory;
